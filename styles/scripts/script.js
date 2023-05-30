815cb1139b9c6a98ca781b4eb992a0c0d4fca70f
andrezgabriel <105998428+andrezgabriel@users.noreply.github.com>
2023-05-30 02:33:55 -0300

    Add files via upload

---
+++ styles/scripts/script.js
@@ -0,0 +1,38 @@
+function calcular(n1,n2) {
+    n1 = parseFloat (document.getElementById("n1").value)
+    n2 = parseFloat (document.getElementById("n2").value)
+
+    selector = document.getElementById("selector").value
+
+    switch(selector) {
+       
+       
+        case '+':
+            calculo = (n1+n2)
+            document.getElementById("resultado").innerHTML = `o resultado da soma de ${n1} + ${n2} é = ${calculo}`
+            break
+
+           
+           
+            case `-`:
+                calculo = (n1 - n2)
+                document.getElementById("resultado").innerHTML = `o resultado da subtração de ${n1} - ${n2} é = ${calculo}`
+                break
+                
+               
+                case `/`:
+                calculo = (n1 / n2)
+                    document.getElementById("resultado").innerHTML = `o resultado da divisão de ${n1} / ${n2} é = ${calculo}`
+                     break
+
+                     case `*`:
+                calculo = (n1 * n2)
+                    document.getElementById("resultado").innerHTML = `o resultado da divisão de ${n1} * ${n2} é = ${calculo}`
+                     break
+
+                     case `%`:
+                        calculo = (n1 % n2)
+                            document.getElementById("resultado").innerHTML = `o resultado da porcentagem de ${n1} % ${n2} é = ${calculo}`
+                             break
+                }
+}
